a08ca9fd496be5bbcdecb17edc3afd5a64a1c346
Harlan Haskins <harlan@bryx.com>
2014-03-02 14:32:17 -0500

    Added calls to remove site folder and zip locally to fabfile.

--- fabfile.py
+++ fabfile.py
@@ -12,6 +12,8 @@ def push():
     run('rm -f /var/www/%s/_site.zip' % (domain))
     run('rm -rf /var/www/%s/_site' % (domain))
     put('_site.zip', '/var/www/%s/_site.zip' % (domain))
+    local("rm -rf _site")
+    local("rm _site.zip")
     run('unzip /var/www/%s/_site.zip -d /var/www/%s' % (domain, domain))
     run('rm -rf /var/www/%s/%s' % (domain, subdom))
     run('mv /var/www/%s/_site /var/www/%s/%s' % (domain, domain, subdom))
